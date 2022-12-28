b74dcd74752d4f4f1bc3fc5cbb5ce9d60b1dcf80
MarcosWinther <winthermarcos@gmail.com>
2022-12-27 21:20:00 -0300

    Projeto em andamento

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photo": "aaaaa",
+    "title": "Saiu um novo filme",
+    "description": "blablabla blabla blablabla"
+  },
+  {
+    "id": 2,
+    "photo": "aaaaa",
+    "title": "Saiu um novo filme",
+    "description": "blablabla blabla blablabla"
+  },
+]
