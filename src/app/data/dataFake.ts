c770d5a73820a66f99e841be9175a0dcd6de888a
MarcosWinther <winthermarcos@gmail.com>
2022-12-28 22:33:50 -0300

    Projeto modificado

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaaaa",
-    "title": "Saiu um novo filme",
-    "description": "blablabla blabla blablabla"
+    "id": "1",
+    "title": "CHIS EVANS, O NOSSO CAPITÃO AMÉRICA",
+    "description": "Marvel anuncia o novo personagem que irá vestir o manto do Capitão América",
+    "photoCover": "https://s2.glbimg.com/_5SrYQMNvjUbxeHEkSPpvdv4jpM=/smart/e.glbimg.com/og/ed/f/original/2022/08/18/capitaoamerica01.jpg"
   },
   {
-    "id": 2,
-    "photo": "aaaaa",
-    "title": "Saiu um novo filme",
-    "description": "blablabla blabla blablabla"
+    "id": "2",
+    "title": "Nova fase da Marvel chegando em breve!",
+    "description": "Fase 5 da Marvel está chegando com tudo!",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/DA2E198288BFCA56AB53340211B38DE7134E40E4521EDCAFE6FFB8CD69250DE9/scale?width=2880&aspectRatio=1.78&format=jpeg"
   },
 ]
